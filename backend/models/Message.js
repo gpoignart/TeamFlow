abf281b0c59c2af4b35f6736bd6f8ab7a7b50a84
ghumeau04 <gauthier.humeau@gmail.com>
2026-01-11 15:44:37 +0700

    Update

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,10 +1,13 @@
 class Message {
-  constructor({ id, content, senderId, recipientIds = [], teamId }) {
+  constructor({ id, content, senderId, recipientIds = [], teamId, reactions = {}, isPinned = false, mentions = [] }) {
     this.id = id;
     this.content = content;
     this.senderId = senderId;
     this.recipientIds = recipientIds;
     this.teamId = teamId || null;
+    this.reactions = reactions; // { userId: emoji }
+    this.isPinned = isPinned;
+    this.mentions = mentions; // array of userIds mentioned
     this.createdAt = new Date().toLocaleString(); // Actual date
   }
 }
